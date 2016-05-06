d445b3ca45077834b69b026cc6a7426de0cb5470
Jared Whiklo <jwhiklo@gmail.com>
2016-05-06 10:33:28 -0500

    Update interface and implementation methods

--- src/KeyCache/RedisKeyCache.php
+++ src/KeyCache/RedisKeyCache.php
@@ -15,97 +15,129 @@ namespace Islandora\Chullo\KeyCache;
  * @author Jared Whiklo <jwhiklo@gmail.com>
  * @since 2016-04-12
  */
-class RedisKeyCache implements IKeyCache {
+class RedisKeyCache implements IUuidCache
+{
   
-  /**
-   * @var string $server_uri
-   *   the hostname of the Redis server.
-   */
-  protected $server_uri;
-  /**
-   * @var int $server_port
-   *   the port of the Redis server.
-   */  
-  protected $server_port;
-  /**
-   * @var int $timeout
-   *   timeout value for communication.
-   */
-  protected $timeout;
-  /**
-   * @var Redis $redis
-   *   The Redis object.
-   */
-  protected $redis;
-  /**
-   * @var bool $connected
-   *   Are we connected to the Redis server yet.
-   */
-  private $connected = False;
+    /**
+     * @var string $server_uri
+     *   the hostname of the Redis server.
+     */
+    protected $server_uri;
+    /**
+     * @var int $server_port
+     *   the port of the Redis server.
+     */
+    protected $server_port;
+    /**
+     * @var int $timeout
+     *   timeout value for communication.
+     */
+    protected $timeout;
+    /**
+     * @var Redis $redis
+     *   The Redis object.
+     */
+    protected $redis;
+    /**
+     * @var bool $connected
+     *   Are we connected to the Redis server yet.
+     */
+    private $connected = false;
   
-  /**
-   * @param string $host
-   *   The hostname of the redis server.
-   * @param int $port
-   *   The port of the redis server.
-   * @param int $timeout
-   *   The timeout value (in seconds), 0 for unlimited.
-   */
-  public function __construct($host, $port, $timeout = 0) {
-    $this->server_uri = $host;
-    $this->server_port = $port;
-    $this->timeout = $timeout;
-    $this->redis = new Redis();
-   
-  }
+    /**
+     * @param string $host
+     *   The hostname of the redis server.
+     * @param int $port
+     *   The port of the redis server.
+     * @param int $timeout
+     *   The timeout value (in seconds), 0 for unlimited.
+     */
+    public function __construct($host, $port, $timeout = 0)
+    {
+        $this->server_uri = $host;
+        $this->server_port = $port;
+        $this->timeout = $timeout;
+        $this->redis = new \Redis();
+    }
   
-  /**
-   * Check if we are connected to the Redis server.
-   *
-   * @return bool Whether we are connected.
-   */
-  private function isConnected() {
-    $connected = False;
-    if ($this->connected) {
-      try {
-        $result = $this->redis->ping();
-        $connected = ($result == "+PONG");
-      } catch (RedisException $e) {
-        error_log("RedisKeyCache: Error communicating with Redis server - " . $e->getMessage());
-      }
+    /**
+     * Check if we are connected to the Redis server.
+     *
+     * @return bool Whether we are connected.
+     */
+    private function isConnected()
+    {
+        $connected = false;
+        if ($this->connected) {
+            try {
+                $result = $this->redis->ping();
+                $connected = ($result == "+PONG");
+            } catch (RedisException $e) {
+                error_log("RedisKeyCache: Error communicating with Redis server - " . $e->getMessage());
+            }
+        }
+        if ($connected === false) {
+            $connected = $this->redis->connect($this->server_uri, $this->server_port, $this->timeout);
+        }
+        return $connected;
     }
-    if ($connected === False) {
-      $connected = $this->redis->connect($this->server_uri, $this->server_port, $this->timeout);
-    }
-    return $connected;
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setKey($key, $value, $expire = 3600) {
-    if ($this->isConnected()) {
-      return $this->redis->setEx($key, $expire, $value);
+    /**
+     * {@inheritdoc}
+     */
+    public function set($txID, $uuid, $path, $expire = 3600)
+    {
+        if ($this->isConnected()) {
+            $result = $this->redis->hSetNx($txID, $uuid, $path);
+            if ($result) {
+                $this->redis->expire($txID, $expire);
+            }
+            return $result;
+        }
+        return false;
     }
-    return False;
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getKey($key) {
-    if ($this->isConnected()) {
-      return $this->redis->get($key);
+    /**
+     * {@inheritdoc}
+     */
+    public function getByUuid($txId, $uuid)
+    {
+        if ($this->isConnected()) {
+            return $this->redis->hget($txId, $uuid);
+        }
     }
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function extend($key, $expire) {
-    if ($this->isConnected()) {
-      return $this->redis->expire($key, $expire);
+    /**
+     * {@inheritdoc}
+     */
+    public function getByPath($txId, $path)
+    {
+        if ($this->isConnected()) {
+            $hashes = $this->redis->hgetall($txId);
+            if ($hashes !== false) {
+                $flipped = array_flip($hashes);
+                if (array_key_exists($path, $hashes)) {
+                    return $hashes[$path];
+                }
+            }
+        }
+        return false;
     }
-  }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function extend($txId, $expire = 3600)
+    {
+        if ($this->isConnected()) {
+            return $this->redis->expire($txId, $expire);
+        }
+    }
+
+    public function delete($txId)
+    {
+        if ($this->isConnected()) {
+            return $this->redis->del($txId);
+        }
+    }
 }
