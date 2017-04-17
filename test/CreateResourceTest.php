53ee287733fdbc1b2a735775e3ed4e6a3c25338f
nruest <ruestn@gmail.com>
2017-04-17 09:40:28 -0400

    TEST; See if removing sections like this effect our code coverage.

--- test/CreateResourceTest.php
+++ test/CreateResourceTest.php
@@ -31,31 +31,4 @@ class CreateResourceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($result->getHeaderLine("Location"), "SOME URI");
         $this->assertEquals(201, $result->getStatusCode(), "Expected a 201 response.");
     }
-    /**
-     * @covers  Islandora\Chullo\FedoraApi::createResource
-     * @uses    GuzzleHttp\Client
-     *
-     * TODO: Is this useful anymore?
-     */
-    public function testReturnsNullOtherwise()
-    {
-        $mock = new MockHandler(
-            [
-            new Response(404),
-            new Response(409),
-            ]
-        );
-
-        $handler = HandlerStack::create($mock);
-        $guzzle = new Client(['handler' => $handler]);
-        $api = new FedoraApi($guzzle);
-
-        // 404
-        $result = $api->createResource("");
-        $this->assertEquals(404, $result->getStatusCode());
-
-        // 409
-        $result = $api->createResource("");
-        $this->assertEquals(409, $result->getStatusCode());
-    }
 }
